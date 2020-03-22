1a21f7afee33f79925b86f933d375ebda4a284d1
Alex Gonzalez <andritogv@gmail.com>
2020-03-22 00:35:12 -0600

    Add Lazy Singleton

---
+++ DesignPatterns.Core/SingletonPattern/LazySingleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace DesignPatterns.Core.SingletonPattern
+{
+    public class LazySingleton
+    {
+    
+        private static Lazy<LazySingleton> instance = new Lazy<LazySingleton>(() => new LazySingleton());
+        
+        private LazySingleton()
+        {
+            WriteLine("Initializing instance");
+        }
+
+        public static LazySingleton Instance => instance.Value;
+    }
+}
